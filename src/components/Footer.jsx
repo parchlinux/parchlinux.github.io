740a619f499a96cfe9e2eef4ed72651993aeb1d6
AFzOficial <abol.mortal50@gmail.com>
2023-08-08 00:06:39 +0330

    add: links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,41 @@
-import { FaGithub, FaDiscord, FaInstagram, FaLinkedinIn, FaTwitter, FaDev, FaTelegramPlane, FaGitlab } from 'react-icons/fa'
-
-
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
+    <footer className="bg-black p-8 flex justify-center shadow-[0_0_70px_theme('colors.sky.950')]">
+      <div className='flex flex-col items-center gap-2'>
+        <div className='flex flex-wrap gap-3 text-xs sm:text-sm'>
 
-    <footer className="p-4 shadow-[0_0_70px_theme('colors.sky.950')]">
-
-      <div className="flex flex-col items-center justify-center sm:flex-row sm:justify-between">
-        <span className="text-sm text-center sm:text-left leading-loose	 font-medium">© 2023 <a target="_blank"
-          href="https://bssinc.ir" className="underline hover:text-sky-500 transition">BSS OSF</a>. All Rights Reserved. <br/>
-
-            <span className="text-xs">
-              Made with ❤️ By <a target="_blank" href="https://pycloud.space" className="underline hover:text-sky-500 transition">AFz.</a>
-            </span>
-        </span>
-
-
-        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-
-
-          <a target="_blank" href="https://github.com/parchlinux" className="hover:bg-zinc-900 p-1 rounded-md">
-            <FaGithub className='w-6 h-6'/>
+          <Link to={'/'} className='hover:text-sky-500'>
+            HOME
+          </Link>
+          <Link to={'/get-parch'} className='hover:text-sky-500'>
+            GET
+          </Link>
+          <a href='https://parchlinux.ir/blog/' target='_blank' className='hover:text-sky-500'>
+            BLOG
           </a>
+          <a href='https://parchlinux.ir/wiki/' target='_blank' className='hover:text-sky-500'>
+            WIKI
+          </a>
+          <Link to={'/about'} className='hover:text-sky-500'>
+            ABOUT
+          </Link>
+          <Link to={'/privacy'} className='hover:text-sky-500'>
+            PRIVACY
+          </Link>
 
         </div>
+
+        <span className='text-sm mt-6'>
+          © 2023 <a target="_blank"
+            href="https://bssinc.ir" className='underline'>BSS OSF</a>. All Rights Reserved.
+        </span>
+
+        <span className="text-xs">
+          Made with ❤️ By <a target="_blank" href="https://pycloud.space" className="underline">AFz.</a>
+        </span>
+
       </div>
     </footer>
 
