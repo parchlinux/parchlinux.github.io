ba83e84ab4c9cd0ab753a01287c6ed2958ccddfb
AFzOficial <abol.mortal50@gmail.com>
2023-08-08 00:06:55 +0330

    change: active link style on sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ export { default as Footer }  from './Footer';
 export { default as Home }  from './home/Home';
 export { default as About }  from './about/About';
 export { default as Team }  from './about/Team';
+export { default as Privacy }  from './about/Privacy';
 export { default as NotFound }  from './error/NotFound';
 export { default as Get }  from './get/Get';
