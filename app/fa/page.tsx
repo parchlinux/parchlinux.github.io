43b10127c1a4371e6c824b8215b721bef0be33ee
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2023-09-08 21:06:56 +0330

    Add & Use vazir font

--- app/fa/page.tsx
+++ app/fa/page.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 
 export default function page() {
     return (
-        <div dir="rtl" className="min-h-screen scroll-smooth overflow-x-hidden bg-black text-white flex flex-col font-yekanBakh">
+        <div dir="rtl" className="min-h-screen scroll-smooth overflow-x-hidden bg-black text-white flex flex-col font-vazir">
             <Nav Lang="Fa" />
             <Main Lang="Fa" />
             <About Lang="Fa" />
