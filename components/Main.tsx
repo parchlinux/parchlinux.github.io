dae77f30cce5ed92d69dbc9fa285be94237bbc22
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2023-09-10 21:49:41 +0330

    Fix hover & Responsive problems

--- components/Main.tsx
+++ components/Main.tsx
@@ -9,7 +9,7 @@ interface propstype {
 export default function Main(props: propstype) {
   return (
     <main className="flex justify-center items-center">
-      <div className="relative flex flex-col md:flex-row justify-between items-center mt-[12vh]">
+      <div className="relative flex flex-col lg:flex-row justify-between items-center mt-[12vh]">
         <Image
           height={9999}
           width={9999}
@@ -24,21 +24,21 @@ export default function Main(props: propstype) {
           alt="notebook"
           className="z-20"
         />
-        <div>
-          <p className="text-8xl hidden md:block">
+        <div className="flex flex-col justify-center items-center z-20">
+          <p className="text-8xl hidden lg:block">
             {props.Lang === "Fa" ? data.Fa.ParchOS : data.En.ParchOS}
           </p>
           <div className="flex justify-center items-center gap-4 mt-10">
             <Link href={"/download"}>
-              <button className="rounded-lg bg-gradient-to-r from-[#0020ee] to-[#1084f2] text-xl px-10 py-3 hover:ring-8 ring-blue-300 cursor-pointer">
+              <button className="rounded-lg bg-gradient-to-r from-[#0020ee] to-[#1084f2] text-xl px-10 py-3 hover:ring-8 ring-blue-300 cursor-pointer truncate">
                 {props.Lang === "Fa" ? data.Fa.download : data.En.download}
               </button>
             </Link>
-            <Link href={"https://wiki.parchlinux.ir/"}>
-              <button className="rounded-lg bg-gradient-to-r from-[#e55026] to-[#eeb200] text-xl px-7 py-3 hover:ring-8 ring-rose-300 cursor-pointer">
+            <a href={"https://wiki.parchlinux.ir/"}>
+              <button className="rounded-lg bg-gradient-to-r from-[#e55026] to-[#eeb200] text-xl px-7 py-3 hover:ring-8 ring-rose-300 cursor-pointer truncate">
                 {props.Lang === "Fa" ? data.Fa.getStarted : data.En.getStarted}
               </button>
-            </Link>
+            </a>
           </div>
         </div>
       </div>
