76d6e2193c1a89ae6deb9ec0dcc02fd75da3f94d
AFzOfficial <abol.mortal50@gmail.com>
2023-12-07 23:47:19 +0330

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="max-w-[860px] flex items-center flex-col mx-auto p-6 min-h-screen">
+        <div class="flex flex-col justify-center px-4 mb-20 mt-10">
+
+            <h1 class="text-3xl sm:text-[45px] font-bold">About Parch Linux</h1>
+
+            <div class='mt-6'>
+                <p class="font-normal text-[15px] leading-7">
+                    Parch Linux is an open-source, Arch-based Linux distribution that aims to provide a user-friendly and aesthetically pleasing computing experience while maintaining a lightweight, fast, and stable operating system. Rooted in the robust foundation of Arch Linux, Parch Linux inherits its renowned stability and performance, ensuring a smooth and reliable user experience. <br/><br/>
+
+                    Parch Linux particularly distinguishes itself through its user-centric approach, catering to both novice and experienced Linux users alike. Its intuitive graphical installer simplifies the installation process, making it accessible to those new to Linux. Additionally, Parch Linux boasts a carefully curated selection of pre-installed applications, including essential tools for productivity, web browsing, multimedia enjoyment, and more, eliminating the need for extensive post-installation configuration. <br/><br/>
+
+                    To further enhance its appeal, Parch Linux offers a choice of desktop environments, including GNOME, KDE Plasma, XFCE, and Cinnamon, empowering users to personalize their computing experience according to their preferences. Each desktop environment provides a unique visual style and set of features, allowing users to select the one that best suits their needs and workflow. <br/><br/>
+
+                    Underneath its user-friendly exterior, Parch Linux leverages the power of Arch Linux's Pacman package manager, renowned for its comprehensiveness and ease of use. With a vast repository of software packages readily available, users can effortlessly expand Parch Linux's capabilities to cater to their specific requirements. <br/><br/>
+
+                    In essence, Parch Linux strikes a harmonious balance between user-friendliness, aesthetics, and performance, making it an ideal choice for those seeking a seamless and enjoyable Linux experience without compromising on stability or flexibility. Its Persian heritage, reflected in its name, adds a touch of cultural richness to the distribution's identity. <br/><br/>
+            </p>
+        </div>
+
+    </div>
+</div></template>
