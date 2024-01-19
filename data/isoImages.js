d7a20fa6fda67f0e316539d763486f92e3143963
AFzOfficial <abol.mortal50@gmail.com>
2024-01-19 23:53:31 +0330

    fix: mirror download urls

--- data/isoImages.js
+++ data/isoImages.js
@@ -1,7 +1,7 @@
 export const isoImages = [
     {
         "name": "plasma",
-        "link": "http://mirror.parchlinux.com/iso/team/Parchlinux.Plasma-2023.12.01-x86_64.iso",
+        "link": "https://github.com/parchlinux/parch-iso-plasma/releases/",
         "sha256": "c355bba7571b2003513568e0a0008144adffea52dba74301bbe2ca58f9e0a4b3",
         "download": true,
         "size": 2,
@@ -9,7 +9,7 @@ export const isoImages = [
     },
     {
         "name": "gnome",
-        "link": "http://mirror.parchlinux.com/iso/team/Parchlinux.Gnome-2023.12.01-x86_64.iso",
+        "link": "https://github.com/parchlinux/Parch-iso-gnome/releases/",
         "sha256": "93d05ec87d63f383c9507df5cee272368f1930fc6003a8ec13ce6f62765ac897",
         "download": true,
         "size": 1.8,
@@ -17,7 +17,7 @@ export const isoImages = [
     },
     {
         "name": "budgie",
-        "link": "http://mirror.parchlinux.com/iso/team/Parchlinux.Budgie-2023.12.01-x86_64.iso",
+        "link": "https://github.com/parchlinux/parch-iso-budgie/releases/",
         "sha256": "6a9fe96840d0a25e7e3ddcb1feeca1521ed3f07a6a42113c12cacc3262e45bec",
         "download": false,
         "size": 1.5,
@@ -25,7 +25,7 @@ export const isoImages = [
     },
     {
         "name": "cinnamon",
-        "link": "http://mirror.parchlinux.com/iso/team/Parchlinux.Cinnamon-2023.12.01-x86_64.iso",
+        "link": "https://github.com/parchlinux/parch-iso-cinnamon/releases/",
         "sha256": "dd5af612619e42c34adda5bd952d1d26d24f3d8d848b09299ac0f317e02b0d18",
         "download": true,
         "size": 1.7,
@@ -33,7 +33,7 @@ export const isoImages = [
     },
     {
         "name": "xfce",
-        "link": "http://mirror.parchlinux.com/iso/team/Parchlinux.XFCE-2023.12.01-x86_64.iso",
+        "link": "https://github.com/parchlinux/parch-iso-xfce/releases/",
         "sha256": "873397fc01273316456bb31ee9f793b191bba7ca3f5bf57e3904c3598958ef17",
         "download": true,
         "size": 1.6,
