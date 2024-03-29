3c3ec073015d6aa61eb93730a6aaec1ad9d80b33
hikari <94219809+metantesan@users.noreply.github.com>
2024-03-29 04:08:49 +0330

    remove snow

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,12 +12,6 @@
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
 import Sidebar from '../components/Sidebar.vue'
-// import "pure-snow.js/style.css"; // Remember to import style.css
-import { createSnow, showSnow } from "pure-snow.js";
-setTimeout(()=>{
-createSnow(); // creates snowflakes and generate css for them
-showSnow(true);
-},1000)
 
 
 export default {
