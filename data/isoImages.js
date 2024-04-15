1a07e7ad1ac6410720af7969d76becaad4213298
hikari <94219809+metantesan@users.noreply.github.com>
2024-04-15 18:55:45 +0330

    Update isoImages.js

--- data/isoImages.js
+++ data/isoImages.js
@@ -15,28 +15,28 @@ export const isoImages = [
         "size": 1.8,
         "photo": "/img/GNOME.webp",
     },
-    {
-        "name": "budgie",
-        "link": "https://github.com/parchlinux/parch-iso-budgie/releases/",
-        "sha256": "6a9fe96840d0a25e7e3ddcb1feeca1521ed3f07a6a42113c12cacc3262e45bec",
-        "download": false,
-        "size": 1.5,
-        "photo": "/img/BUDGIE.webp",
-    },
-    {
-        "name": "cinnamon",
-        "link": "https://github.com/parchlinux/parch-iso-cinnamon/releases/",
-        "sha256": "dd5af612619e42c34adda5bd952d1d26d24f3d8d848b09299ac0f317e02b0d18",
-        "download": true,
-        "size": 1.7,
-        "photo": "/img/CINNAMON.webp",
-    },
-    {
-        "name": "xfce",
-        "link": "https://github.com/parchlinux/parch-iso-xfce/releases/",
-        "sha256": "873397fc01273316456bb31ee9f793b191bba7ca3f5bf57e3904c3598958ef17",
-        "download": true,
-        "size": 1.6,
-        "photo": "/img/XFCE.webp",
-    }
+    // {
+    //     "name": "budgie",
+    //     "link": "https://github.com/parchlinux/parch-iso-budgie/releases/",
+    //     "sha256": "6a9fe96840d0a25e7e3ddcb1feeca1521ed3f07a6a42113c12cacc3262e45bec",
+    //     "download": false,
+    //     "size": 1.5,
+    //     "photo": "/img/BUDGIE.webp",
+    // },
+    // {
+    //     "name": "cinnamon",
+    //     "link": "https://github.com/parchlinux/parch-iso-cinnamon/releases/",
+    //     "sha256": "dd5af612619e42c34adda5bd952d1d26d24f3d8d848b09299ac0f317e02b0d18",
+    //     "download": true,
+    //     "size": 1.7,
+    //     "photo": "/img/CINNAMON.webp",
+    // },
+    // {
+    //     "name": "xfce",
+    //     "link": "https://github.com/parchlinux/parch-iso-xfce/releases/",
+    //     "sha256": "873397fc01273316456bb31ee9f793b191bba7ca3f5bf57e3904c3598958ef17",
+    //     "download": true,
+    //     "size": 1.6,
+    //     "photo": "/img/XFCE.webp",
+    // }
 ]
